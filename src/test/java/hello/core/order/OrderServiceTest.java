ee9fc91621522896df910c27797ac581763ac9a5
hong-brother <robbins.hs@gmail.com>
2022-03-11 22:05:57 +0900

    add lombok, custom annotation

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -28,7 +28,6 @@ public class OrderServiceTest {
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
-
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
